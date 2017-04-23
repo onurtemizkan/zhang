27981dc96e7bd9168a2465872554335c0b059263
onurtemizkan <onurtemizkan@gmail.com>
2017-04-23 17:19:24 +0300

    Add timer utility

---
+++ utils/timer.py
@@ -0,0 +1,13 @@
+import time
+import inspect
+
+
+def timer():
+    start = time.time()
+
+    def end(method_name="Unnamed function"):
+
+        print method_name + " took : " + str(time.time() - start) + " seconds."
+        return
+
+    return end
